1b5869ece8eac00127a369ffd46a0206e8adf86d
DMC <surendra.singh20.in@gmail.com>
2024-11-21 09:26:03 +0700

    Update Database_Creation.sh

--- Database_Creation.sh
+++ Database_Creation.sh
@@ -111,9 +111,6 @@ INSERT IGNORE INTO check_imei_response_param (tag, value, feature_name, language
 INSERT IGNORE INTO eirs_response_param (tag, value, feature_name, language) VALUES ('customImeiRegisterReqLength0', 'JSON Array length is 0', 'CustomCheckImei', 'en');
 INSERT IGNORE INTO eirs_response_param (tag, value, feature_name, language) VALUES ('customImeiRegisterPayLoadMaxSize', 'JSON Array length is greater than MAX Allowed IMEI count', 'CustomCheckImei', 'en');
  
-INSERT IGNORE INTO sys_param (tag, value, feature_name) VALUES ('CustomApiAuthOperatorCheck', 'false', 'CustomCheckImei');
-INSERT IGNORE INTO sys_param (tag, value, feature_name) VALUES ('CustomApiAuthWithIpCheck', 'false', 'CustomCheckImei');
-
 INSERT IGNORE INTO feature_rule (feature, grace_action, name, post_grace_action, rule_order, user_type, failed_rule_action_grace, failed_rule_action_post_grace, output) VALUES ('CustomCheckImei', 'Skip', 'CUSTOM_GDCE', 'Skip', 1, 'default', 'Record', 'Record', 'NO');
 INSERT IGNORE INTO feature_rule (feature, grace_action, name, post_grace_action, rule_order, user_type, failed_rule_action_grace, failed_rule_action_post_grace, output) VALUES ('CustomCheckImei', 'Skip', 'EXIST_IN_BLACKLIST_DB', 'Skip', 2, 'default', 'Record', 'Record', 'NO');
 INSERT IGNORE INTO feature_rule (feature, grace_action, name, post_grace_action, rule_order, user_type, failed_rule_action_grace, failed_rule_action_post_grace, output) VALUES ('CustomCheckImei', 'Skip', 'GREYLIST_BY_LOST_STOLEN', 'Skip', 3, 'default', 'Record', 'Record', 'NO');
@@ -132,20 +129,23 @@ INSERT IGNORE INTO rule (name, output, state) VALUES ('INVALID_IMEI', 'No', 'Ena
 INSERT IGNORE INTO rule (name, output, state) VALUES ('CUSTOM_GDCE', 'No', 'Enabled');
 INSERT IGNORE INTO rule (name, output, state) VALUES ('GREYLIST_BY_LOST_STOLEN', 'No', 'Enabled');
 
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CustomAuthApiUrlPath', 'https://stage-gateway.customs.gov.kh/dmc-interface/api/v1/oauth/token', 'CustomCheckImei' );
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CustomAuthApiClientId', '2', 'CustomCheckImei' );
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CustomAuthApiSecretKey', 'QNzhRKixNfStVkomv5S1XsQdgb3ufgAiktF2wPMz', 'CustomCheckImei');
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CustomCheckImeiApiUrlPath', 'https://stage-gateway.customs.gov.kh/dmc-interface/api/v1/imei-tax-check', 'CustomCheckImei' );
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CamDxLayerHeaderName', 'X-Road-Client', 'CustomCheckImei');
-INSERT IGNORE INTO sys_param (tag, value, feature_name  ) VALUES ('CamDxLayerHeaderValue', 'CAMBODIA/GOV/CAMDX-20201222/GDCE_EIRS_TEST', 'CustomCheckImei');
+INSERT IGNORE INTO sys_param (tag, value, feature_name ,type,active,remark,user_type,modified_by  ) VALUES ('CustomAuthApiUrlPath', 'https://stage-gateway.customs.gov.kh/dmc-interface/api/v1/oauth/token', 'CustomCheckImei',0,1,'','','' );
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by   ) VALUES ('CustomAuthApiClientId', '2', 'CustomCheckImei',0,1,'','','' );
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by   ) VALUES ('CustomAuthApiSecretKey', 'QNzhRKixNfStVkomv5S1XsQdgb3ufgAiktF2wPMz', 'CustomCheckImei',0,1,'','','');
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by   ) VALUES ('CustomCheckImeiApiUrlPath', 'https://stage-gateway.customs.gov.kh/dmc-interface/api/v1/imei-tax-check', 'CustomCheckImei',0,1,'','','' );
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by   ) VALUES ('CamDxLayerHeaderName', 'X-Road-Client', 'CustomCheckImei',0,1,'','','');
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by   ) VALUES ('CamDxLayerHeaderValue', 'CAMBODIA/GOV/CAMDX-20201222/GDCE_EIRS_TEST', 'CustomCheckImei',0,1,'','','');
 
-insert IGNORE into sys_param(tag , value,feature_name)values ( 'gdce_register_imei_update_last_run_time' , '2020-01-01' , 'CustomCheckImei');
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by ) VALUES ('CustomApiFilePath', '/u02/eirsdata/GdceFiles/','CustomCheckImei',0,1,'','','');
+INSERT IGNORE INTO sys_param (tag, value,feature_name,type,active,remark,user_type,modified_by ) VALUES ('alert_api_url_path','http://64.227.146.191:9509/eirs/alert', 'Alert',0,1,'','','');
+INSERT IGNORE INTO sys_param(tag, value, feature_name,type,active,remark,user_type,modified_by ) VALUES ('platFormComponentApiUrl', 'http://64.227.146.191:9509/eirs', 'CustomCheckImei',0,1,'','','');
+
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by ) VALUES ('CustomApiAuthOperatorCheck', 'false', 'CustomCheckImei',0,1,'','','');
+INSERT IGNORE INTO sys_param (tag, value, feature_name,type,active,remark,user_type,modified_by ) VALUES ('CustomApiAuthWithIpCheck', 'false', 'CustomCheckImei',0,1,'','','');
+
+insert IGNORE into sys_param(tag , value,feature_name ,type,active,remark,user_type,modified_by )values ( 'gdce_register_imei_update_last_run_time' , '2020-01-01' , 'CustomCheckImei',0,1,'','','');
 insert IGNORE into cfg_feature_alert( alert_id,description,feature )values ('alert1607','Exception <e> occurred for <process_name>.' , 'RegisterCustomImeiUpdate');
 
-
-INSERT IGNORE INTO sys_param (tag, value, feature_name) VALUES ('CustomApiFilePath', '/u02/eirsdata/GdceFiles/','CustomCheckImei');
-INSERT IGNORE INTO sys_param (tag, value,feature_name) VALUES ('alert_api_url_path','http://64.227.146.191:9509/eirs/alert', 'Alert');
-INSERT IGNORE INTO sys_param(tag, value, feature_name) VALUES ('platFormComponentApiUrl', 'http://64.227.146.191:9509/eirs', 'CustomCheckImei');
 
 alter table gdce_data add column sim int default 0, add column brand varchar(255) default '', add column model varchar(255) default '', add column device_type varchar(50) default '', add column goods_description varchar(255) default '', drop column transaction_id  , modify column serial_number varchar(50) default '';
 
